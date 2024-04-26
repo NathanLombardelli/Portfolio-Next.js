d52c69818c49c9ab260262224c374e157bdf8a17
Nathan Lombardelli <nathanlombardelli@hotmail.com>
2024-04-27 01:43:45 +0200

    nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
+"use client"
+import './global.scss';
+import Spline from '@splinetool/react-spline';
+import Nav from "../components/nav/Nav";
+
 
 export default function Home() {
-  return (
-    <main>
 
-      <h1>Nathan Lombardelli</h1>
+    return (
+        <main>
+            <div className={'containerHeader'}>
+                <div>
+                    <h1>Nathan Lombardelli</h1>
+                </div>
+                <div>
+                </div>
+                <div>
+                    <h1>Nathan Lombardelli</h1>
+                </div>
 
-    </main>
-  );
+            </div>
+            <Spline style={{height: '100vh'}} scene="https://prod.spline.design/S8kIvME2BM9NVwu8/scene.splinecode"/>
+
+            <Nav/>
+
+        </main>
+    );
 }
---
+++ src/components/nav/Nav.tsx
@@ -0,0 +1,64 @@
+import './Nav.scss';
+import {useEffect, useRef, useState} from "react";
+
+export default function Nav() {
+
+    const [selectionWidth, setSelectionWidth] = useState<number>(0);
+    const [selectionPos, setSelectionPos] = useState<number>(0);
+    const [isHover, setIsHover] = useState(false);
+
+    const comp = useRef<HTMLLIElement>(null);
+    const exp = useRef<HTMLLIElement>(null);
+    const cv = useRef<HTMLLIElement>(null);
+    const con = useRef<HTMLLIElement>(null);
+    const navRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const elements = [comp, exp, cv, con];
+        elements.forEach(ref => {
+            // @ts-ignore
+            ref.current?.addEventListener('mouseover', () => updateSelection(ref.current));
+            ref.current?.addEventListener('mouseout', handleMouseOut);
+        });
+
+        return () => {
+            elements.forEach(ref => {
+                // @ts-ignore
+                ref.current?.removeEventListener('mouseover', () => updateSelection(ref.current));
+                ref.current?.removeEventListener('mouseout', handleMouseOut);
+            });
+        };
+
+    }, []);
+
+
+    const handleMouseOut = () => {
+        setIsHover(false);
+    };
+    const updateSelection = (element: HTMLLIElement) => {
+        const rect = element.getBoundingClientRect();
+        const navRect = navRef.current?.getBoundingClientRect();
+        if (navRect) {
+            setSelectionWidth(rect.width);
+            setSelectionPos((rect.left - navRect.left));
+            setIsHover(true);
+        }
+    }
+
+
+    return (
+        <nav ref={navRef} className={'nav'}>
+            <ul>
+                <li ref={comp}><a>Compétances</a></li>
+                <li ref={exp}><a>Expérience</a></li>
+                <li ref={cv}><a>CV</a></li>
+                <li ref={con}><a>Contact</a></li>
+            </ul>
+            <div style={{
+                width: `${selectionWidth}px`,
+                transform: `translateX(${selectionPos}px) scale(${isHover ? 1 : 0})`,
+                opacity: isHover ? 1 : 0
+            }} className={'navSelection'}/>
+        </nav>
+    );
+}
